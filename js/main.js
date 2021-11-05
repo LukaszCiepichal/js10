8b7129e9a8a70ba40911b77f1ee8b2deab7932c9
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-05 11:48:18 +0100

    dlugosc stringa

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let napis1 = 'Uwielbiam JavaScript';
+let napis2 = 'Jestem świetnym programistą';
+
+function ktoryWiekszy(txt1, txt2) {
+    
+    if (txt1.length > txt2.length) {
+        return txt1
+
+    }
+}
+
+console.log(ktoryWiekszy(napis2, napis1));
