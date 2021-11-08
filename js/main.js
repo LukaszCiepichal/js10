7b4caba16487b823fdc8c2c7183b97217de34282
lukasz ciepichal <lukaszciepichal1@gmail.com>
2021-11-08 16:51:48 +0100

    poprawa funkcji

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-let napis1 = 'Uwielbiam JavaScript';
-let napis2 = 'Jestem świetnym programistą';
+let napis1 = 'Uwielbiam JavaScript!';
+let napis2 = 'Jestem świetnym programistą.';
 
 function ktoryWiekszy(txt1, txt2) {
     
     if (txt1.length > txt2.length) {
-        return txt1
-
+        return txt1;
+    } else {
+        return txt2;
     }
+        
+        
+        
+
+    
 }
 
-console.log(ktoryWiekszy(napis2, napis1));
+console.log(ktoryWiekszy(napis1, napis2));
